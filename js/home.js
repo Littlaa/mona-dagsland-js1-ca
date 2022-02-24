a43c28f97c7630a27da04dcd559b17128dd9dabd
Littlaa <monadags@gmail.com>
2022-02-24 19:39:25 +0100

    almost finished with index

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-const url = "https://omgvamp-hearthstone-v1.p.rapidapi.com/info";
+const url = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cardbacks";
 const key = {
   headers: {
     "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
@@ -6,10 +6,31 @@ const key = {
   },
 };
 
+const resultsContainer = document.querySelector(".results");
+
 async function callHeartstone() {
-  const response = await fetch(url, key);
-  const results = await response.json();
-  console.log(results);
+  try {
+    const response = await fetch(url, key);
+    const results = await response.json();
+    const cards = results;
+
+    setTimeout(function () {
+      resultsContainer.innerHTML = "";
+
+      for (let i = 0; i < cards.length; i++) {
+        resultsContainer.innerHTML += `<div class="result">
+                                        <p>Card ID: ${cards[i].cardBackId}</p>
+                                        <p>Name: ${cards[i].name}</p>
+                                        <p>Category: ${cards[i].sortCategory}</p>
+                                        </div>`;
+      }
+    }, 1000);
+  } catch (error) {
+    console.log(error);
+    resultsContainer.innerHTML = displayError("Something went wrong while calling the API");
+  }
 }
 
 callHeartstone();
+
+//<img src=${cards[i].img} tried this in the inner.html but the images wouldn't show because of access denied.
