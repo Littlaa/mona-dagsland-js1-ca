d9d6bf0a2c1b9ee97452bc981116780170c53e12
Littlaa <monadags@gmail.com>
2022-02-24 22:15:28 +0100

    some fixes to the code

--- js/home.js
+++ js/home.js
@@ -18,11 +18,14 @@ async function callHeartstone() {
       resultsContainer.innerHTML = "";
 
       for (let i = 0; i < cards.length; i++) {
-        resultsContainer.innerHTML += `<div class="result">
+        if (i === 12) {
+          break;
+        }
+        resultsContainer.innerHTML += `<a href="details.html?cardBackId=${cards[i].cardBackId}" class="result">
                                         <p>Card ID: ${cards[i].cardBackId}</p>
                                         <p>Name: ${cards[i].name}</p>
                                         <p>Category: ${cards[i].sortCategory}</p>
-                                        </div>`;
+                                        </a>`;
       }
     }, 1000);
   } catch (error) {
@@ -34,3 +37,4 @@ async function callHeartstone() {
 callHeartstone();
 
 //<img src=${cards[i].img} tried this in the inner.html but the images wouldn't show because of access denied.
+//I had 314 API calls so I took it down to 12 since we had to make details page for every results. Hope that was OK.
